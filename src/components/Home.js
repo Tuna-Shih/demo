a46acfa1e6b092b53cc4c660132dde1b947aa20f
Tuna-Shih <61424827+Tuna-Shih@users.noreply.github.com>
2020-09-26 01:30:25 +0800

    feat: less

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,15 @@ import React from 'react';
 import ToolBar from './ToolBar';
 import UserInformation from './UserInformation';
 import Footer from './Footer';
+import styles from './styles/Home.less';
 import './styles/Global.less';
 
 const Home = () => (
-  <>
+  <div className={styles.root}>
     <ToolBar />
     <UserInformation />
     <Footer />
-  </>
+  </div>
 );
 
 export default React.memo(Home);
--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -11,25 +11,27 @@ import {
 import styles from './styles/ToolBar.less';
 
 const ToolBar = () => (
-  <Row className={styles.root}>
-    <Col span={8}>
-      <img src="/Instagram.png" alt="IG" />
-    </Col>
+  <div className={styles.root}>
+    <Row>
+      <Col span={8}>
+        <img src="/Instagram.png" alt="IG" />
+      </Col>
 
-    <Col span={8}>
-      <AutoComplete className={styles.searchBar} placeholder="🔍搜尋" />
-    </Col>
+      <Col span={8}>
+        <AutoComplete className={styles.searchBar} placeholder="🔍搜尋" />
+      </Col>
 
-    <Col span={8}>
-      <div className={styles.tools}>
-        <HomeOutlined />
-        <SendOutlined />
-        <CompassOutlined />
-        <HeartOutlined />
-        <UserOutlined />
-      </div>
-    </Col>
-  </Row>
+      <Col span={8}>
+        <div className={styles.tools}>
+          <HomeOutlined />
+          <SendOutlined />
+          <CompassOutlined />
+          <HeartOutlined />
+          <UserOutlined />
+        </div>
+      </Col>
+    </Row>
+  </div>
 );
 
 export default React.memo(ToolBar);
